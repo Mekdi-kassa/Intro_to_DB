6df1af844657897feb2101c09b74fa4ece5d257a
Medkelawit_Abadina <mekdelawitkassa6@gmail.com>
2025-08-17 15:50:42 +0300

    fix

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE 'alx_book_store';
+CREATE DATABASE IF NOT EXISTS'alx_book_store';
 
 USE 'alx_book_store';
 
@@ -6,7 +6,7 @@ CREATE TABLE Authors(
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
-create TABLE Books(
+CREATE TABLE Books(
     book_id INT AUTO_INCREMENT  Primary key,
     title VARCHAR(130) NOT NULL,
     author_id INT,
