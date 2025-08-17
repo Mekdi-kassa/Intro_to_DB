7533a8cbd482dc2b584000d91d25c66d5ee02b88
Medkelawit_Abadina <mekdelawitkassa6@gmail.com>
2025-08-17 15:58:25 +0300

    Table and DataBase creation

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -31,7 +31,7 @@ CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity DECIMAL(10,2),
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
