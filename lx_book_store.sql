c1a23f222102e4d28f6d9289c213656c76624fa5
Medkelawit_Abadina <mekdelawitkassa6@gmail.com>
2025-08-17 15:47:28 +0300

    Table and Data_Base creations

---
+++ lx_book_store.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE 'alx_book_store';
+
+USE 'alx_book_store';
+
+CREATE TABLE Authors(
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+create TABLE Books(
+    book_id INT AUTO_INCREMENT  Primary key,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DECIMAL(10,2),
+    publication _date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+CREATE TABLE Customers(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address Text
+);
+CREATE TABLE Orders(
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE NOT NULL,
+    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+);
+CREATE TABLE Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DECIMAL(10,2),
+    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY(book_id) REFERENCES Books(book_id)
+);
